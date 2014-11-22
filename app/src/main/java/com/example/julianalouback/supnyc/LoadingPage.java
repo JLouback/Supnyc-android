c363a6ea4adb32773dab18f67af1b6f17c878ddc
JLouback <juliana.m.louback@gmail.com>
2014-11-22 12:28:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/julianalouback/supnyc/LoadingPage.java
@@ -0,0 +1,72 @@
+package com.example.julianalouback.supnyc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+
+public class LoadingPage extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+        //Initialize a LoadViewTask object and call the execute() method
+        new LoadViewTask().execute();
+
+    }
+
+    //To use the AsyncTask, it must be subclassed
+    private class LoadViewTask extends AsyncTask<Void, Integer, Void>
+    {
+        //Before running code in separate thread
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+
+        //The code to be executed in a background thread.
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            /* This is just a code that delays the thread execution 4 times,
+             * during 850 milliseconds and updates the current progress. This
+             * is where the code that is going to be executed on a background
+             * thread must be placed.
+             */
+            try
+            {
+                //Get the current thread's token
+                synchronized (this)
+                {
+                    //Initialize an integer (that will act as a counter) to zero
+                    int counter = 0;
+                    //While the counter is smaller than four
+                    while(counter <= 4)
+                    {
+                        //Wait 850 milliseconds
+                        this.wait(850);
+                    }
+                }
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        //after executing the code in the thread
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            //initialize the View
+            Intent intent = new Intent(LoadingPage.this, com.example.julianalouback.supnyc.Menu.class);
+        }
+    }
+
+}
